a842ca191ddbbf87f5e29554077d906b958a73cd
UNILORN <b5159@oic.jp>
2016-08-24 06:51:17 +0000

    indent

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
 var flug = 0;
-$(function(){
-  var contentfirst = $('.content-first').offset().top
-  var contentsecond = $('.content').offset().top
-  $(window).scroll(function(){
-    var top = $(window).scrollTop();
-    if( top <= contentfirst && flug != 0){
-      flug = 0;
-      $(".image-1").fadeIn();
-      $(".image-2").fadeOut();
-    }
-    if( top > contentfirst && flug != 1){
-      flug = 1;
-      $(".image-1").fadeOut();
-      $(".image-2").fadeIn();
-    }
-  });
+$(function() {
+    var contentfirst = $('.content-first').offset().top
+    var contentsecond = $('.content').offset().top
+    $(window).scroll(function() {
+        var top = $(window).scrollTop();
+        if (top <= contentfirst && flug != 0) {
+            flug = 0;
+            $(".image-1").fadeIn();
+            $(".image-2").fadeOut();
+        }
+        if (top > contentfirst && flug != 1) {
+            flug = 1;
+            $(".image-1").fadeOut();
+            $(".image-2").fadeIn();
+        }
+    });
 
-  $('.content-first-inner').box2fullWidth({
-  minwidth:400,
-  who:'div',
-  margin:40,
-  duration:200,
-  boxdelay:0,
-  bottomMargin:50,
-  positionComplete:function(){ }
-});
+    $('.content-first-inner').box2fullWidth({
+        minwidth: 400,
+        who: 'div',
+        margin: 40,
+        duration: 200,
+        boxdelay: 0,
+        bottomMargin: 50,
+        positionComplete: function() {}
+    });
 
-  $(".content-box").hover(function(){
-    $(this).find(".content-box-lay").fadeIn();
-    $(this).find(".content-box-lay").css("display","table");
-    $(this).find(".content-box-inner").fadeIn();
-    var num = $(this).find("input").val();
-    var text = ".content-fixed-num" + num;
-    $(text).show('slide',300);
-    $(text).css("display","table");
-  },function(){
-    $(this).find(".content-box-lay").fadeOut();
-    $(this).find(".content-box-inner").fadeOut();
-    var num = $(this).find("input").val();
-    var text = ".content-fixed-num" + num;
-    $(text).hide('slide',300);
-  });
-
-
-
-
-
+    $(".content-box").hover(function() {
+        $(this).find(".content-box-lay").fadeIn();
+        $(this).find(".content-box-lay").css("display", "table");
+        $(this).find(".content-box-inner").fadeIn();
+        var num = $(this).find("input").val();
+        var text = ".content-fixed-num" + num;
+        $(text).show('slide', 500);
+        $(text).css("display", "table");
+    }, function() {
+        $(this).find(".content-box-lay").fadeOut();
+        $(this).find(".content-box-inner").fadeOut();
+        var num = $(this).find("input").val();
+        var text = ".content-fixed-num" + num;
+        $(text).hide('slide', 500);
+    });
 });
